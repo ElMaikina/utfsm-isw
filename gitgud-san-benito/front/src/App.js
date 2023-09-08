aa46475e4f368d41c18aadac50378d525c383c0c
ElMaikina <mikesoto890@gmail.com>
2023-09-07 22:30:09 -0300

    new app.js

--- gitgud-san-benito/front/src/App.js
+++ gitgud-san-benito/front/src/App.js
@@ -2,24 +2,58 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const [rut, setRut] = useState('');
+  const [patent, setPatent] = useState('');
+  const [desc, setDesc] = useState('');
+  const [severidad, setSev] = useState('leve');
+  const [model, setModel] = useState('nunoki');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="create">
+      <h2>Reportar Infraccion</h2>
+      <form>
+        <label>RUT Infractor:</label>
+        <input 
+          type="text" 
+          required 
+          value={rut}
+          onChange={(e) => setRut(e.target.value)}
+        />
+        <label>Patente del vehiculo:</label>
+        <input 
+          type="text" 
+          required 
+          value={patent}
+          onChange={(e) => setPatent(e.target.value)}
+        />
+        <label>Modelo del vehiculo:</label>
+        <input 
+          type="text" 
+          required 
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+        />
+        <label>Descripcion de la Infraccion:</label>
+        <textarea
+          required
+          value={desc}
+          onChange={(e) => setDesc(e.target.value)}
+        ></textarea>
+        <label>severidad:</label>
+        <select
+          value={severidad}
+          onChange={(e) => setSev(e.target.value)}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="leve">Leve</option>
+          <option value="moderada">Moderada</option>
+          <option value="grave">Grave</option>
+        </select>
+        <button>Entregar</button>
+      </form>
     </div>
   );
+  
 }
 
 export default App;
