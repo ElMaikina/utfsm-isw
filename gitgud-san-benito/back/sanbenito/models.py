2ed41c468c05f24954685bb26a4466a35b4e2b0f
ElMaikina <mikesoto890@gmail.com>
2023-10-06 20:39:36 -0300

    backend can connect to db

--- gitgud-san-benito/back/sanbenito/models.py
+++ gitgud-san-benito/back/sanbenito/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Carabinero(models.Model):
+    rut=models.CharField(max_length=7)
     nombre=models.CharField(max_length=100)
     def __str__(self):
         return self.name
