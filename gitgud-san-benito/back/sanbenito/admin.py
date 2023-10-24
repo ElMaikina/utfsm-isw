c972aa35d26ba321116012d9fd2b4aa7bcae1b5a
ElMaikina <mikesoto890@gmail.com>
2023-10-23 23:33:52 -0300

    finished rest api in backend

--- gitgud-san-benito/back/sanbenito/admin.py
+++ gitgud-san-benito/back/sanbenito/admin.py
@@ -2,14 +2,12 @@ from django.contrib import admin
 from django.db import models
 
 # Register your models here.
-from .models import Persona
 from .models import Carabinero
 from .models import Inspector
 from .models import Infractor
 from .models import Juez
 from .models import Infraccion
 
-admin.site.register(Persona)
 admin.site.register(Carabinero)
 admin.site.register(Inspector)
 admin.site.register(Infractor)
--- gitgud-san-benito/back/sanbenito/models.py
+++ gitgud-san-benito/back/sanbenito/models.py
@@ -1,31 +1,32 @@
 from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import render
+from django.utils import timezone
 from django.db import models
 
 # Modelo base de una persona
 class Persona(models.Model):
-    nombres = models.CharField(max_length=255)
-    apellidos = models.CharField(max_length=255)
-    rut = models.CharField(max_length=9, primary_key=True)  # Aquí estamos usando un campo CharField para representar el RUT
-    fecha_de_nacimiento = models.DateField()
+    nombres = models.CharField(max_length=255, null=True)
+    apellidos = models.CharField(max_length=255, null=True)
+    rut = models.IntegerField(max_length=9, primary_key=True)  # Aquí estamos usando un campo CharField para representar el RUT
+    fecha_de_nacimiento = models.DateField(default=timezone.now, null=False)
 
     def __str__(self):
         return f"{self.nombres} {self.apellido}"
 
 # Personas encargadas de emitir los partes
-class Carabinero(Persona):
-    rango = models.CharField(max_length=50)
+class Carabinero(models.Model):
+    rango = models.CharField(max_length=50, null=True)
 
-class Inspector(Persona):
-    area_de_inspeccion = models.CharField(max_length=100)
+class Inspector(models.Model):
+    area_de_inspeccion = models.CharField(max_length=100, null=True)
 
 # Persona que comete una infraccion
-class Infractor(Persona):
-    numero_de_infracciones = models.PositiveIntegerField()
+class Infractor(models.Model):
+    numero_de_infracciones = models.PositiveIntegerField(default=0)
 
 # Persona que juzga el estado de una infraccion
-class Juez(Persona):
-    experiencia_judicial = models.PositiveIntegerField()
+class Juez(models.Model):
+    experiencia_judicial = models.PositiveIntegerField(default=0)
 
 # El ticket generado por un carabinero o inspector hacia un infractor
 class Infraccion(models.Model):
