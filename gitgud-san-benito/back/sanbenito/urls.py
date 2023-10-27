3ad154bd730a884b0e0232dce55d28408706285f
ElMaikina <mikesoto890@gmail.com>
2023-10-26 23:39:16 -0300

    connected front o back

--- gitgud-san-benito/back/sanbenito/urls.py
+++ gitgud-san-benito/back/sanbenito/urls.py
@@ -5,8 +5,8 @@ from .models import *
 from .views import *
 
 post_router = DefaultRouter()
-post_router.register(r'ver_carabineros', VerCarabineros)
-post_router.register(r'ver_inspectores', VerInspectores)
-post_router.register(r'ver_infractores', VerInfractores)
-post_router.register(r'ver_infracciones', VerInfracciones)
-post_router.register(r'ver_evidencia', VerEvidencia)
+post_router.register(r'carabineros', Carabineros)
+post_router.register(r'inspectores', Inspectores)
+post_router.register(r'infractores', Infractores)
+post_router.register(r'infracciones', Infracciones)
+post_router.register(r'evidencia', Evidencias)
--- gitgud-san-benito/back/sanbenito/views.py
+++ gitgud-san-benito/back/sanbenito/views.py
@@ -15,52 +15,26 @@ from rest_framework.renderers import HTMLFormRenderer, JSONRenderer, BrowsableAP
 # Usa el API REST para conectarse con React
 
 # Muestra todos los carabineros
-class VerCarabineros(ModelViewSet):
+class Carabineros(ModelViewSet):
     queryset = Carabinero.objects.all()
     serializer_class = CarabineroSerial
 
 # Muestra todos los inspectores
-class VerInspectores(ModelViewSet):
+class Inspectores(ModelViewSet):
     queryset = Inspector.objects.all()
     serializer_class = InspectorSerial
 
 # Muestra todos los infractores
-class VerInfractores(ModelViewSet):
+class Infractores(ModelViewSet):
     queryset = Infractor.objects.all()
     serializer_class = InfractorSerial
 
 # Muestra todos las infracciones
-class VerInfracciones(ModelViewSet):
+class Infracciones(ModelViewSet):
     queryset = Infraccion.objects.all()
     serializer_class = InfraccionSerial
 
 # Muestra todos las infracciones
-class VerEvidencia(ModelViewSet):
+class Evidencias(ModelViewSet):
     queryset = Evidencia.objects.all()
     serializer_class = EvidenciaSerial
-
-# Muestra la pagina principal de backend
-def index(request):
-    print("Bienvenido al Back-End de San Benito!\n")
-    
-    models = apps.get_models()
-    all_views = []
-
-    for key, resolver in get_resolver().reverse_dict.items():
-        if isinstance(resolver[0][0], str):
-            all_views.append(key)
-
-    # Muestra todos las entidades y sus campos por terminal
-    print("Modelos:")
-    for model in models:
-        print(" - " + model.__name__)
-        for campo in model._meta.get_fields():
-            print("   - " + campo.name + ": " + campo.__class__.__name__)
-
-    # Muestra todas las vistas por terminal
-    print("Vistas:")
-    for view in all_views:
-        print(" - " + view)
-
-    return render(request, 'index.html', {'models': models, 'all_views': all_views})
-    
